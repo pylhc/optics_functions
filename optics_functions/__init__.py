1009146fa60a3533ee844b20e7024a6997265da3
Felix Soubelet <felix.soubelet@protonmail.com>
2021-01-27 10:25:55 +0100

    rework setup.py and add __init__.py files (#1)

---
+++ optics_functions/__init__.py
@@ -0,0 +1,14 @@
+"""
+Accelerator optics related calculations, directly from tfs files.
+"""
+
+__title__ = "optics_functions"
+__description__ = "Calculate optics parameters from TWISS outputs."
+__url__ = "https://github.com/pylhc/optics_functions"
+__version__ = "0.1.0"
+__author__ = "pylhc"
+__author_email__ = "pylhc@github.com"
+__license__ = "MIT"
+
+
+__all__ = []
