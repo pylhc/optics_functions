2be6f18fa585349c6c481e839c49fe46900159d1
Felix Soubelet <felix.soubelet@protonmail.com>
2024-07-25 16:21:38 +0200

    and bump patch version for release

--- optics_functions/__init__.py
+++ optics_functions/__init__.py
@@ -5,7 +5,7 @@ Accelerator optics related calculations, directly from tfs files.
 __title__ = "optics_functions"
 __description__ = "Calculate optics parameters from TWISS outputs."
 __url__ = "https://github.com/pylhc/optics_functions"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "pylhc"
 __author_email__ = "pylhc@github.com"
 __license__ = "MIT"
